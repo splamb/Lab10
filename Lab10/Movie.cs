f8fa77980e0652c81da87b15568f2115c0e7bed8
Sean L <splambert1020@gmail.com>
2021-09-01 16:05:53 -0400

    first commit

---
+++ Lab10/Movie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    public class Movie
+    {
+        private string _title;
+        private string _category;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public Movie (string title, string category)
+        {
+            Setup(title, category);
+        }
+
+        private void Setup(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cont = "y";
+
+            var alien = new Movie("Alien", "horror");
+            var benjaminButton = new Movie("The Curious Case of Benjamin Button", "drama");
+            var starWars = new Movie("Star Wars Episode IV: A New Hope", "scifi");
+            var cars = new Movie("Cars", "animated");
+            var insidious = new Movie("Insidious", "horror");
+            var casablanca = new Movie("Casablanca", "drama");
+            var dune = new Movie("Dune", "scifi");
+            var toyStory = new Movie("Toy Story", "animated");
+            var angryMen = new Movie("12 Angry Men", "drama");
+            var theMatrix = new Movie("The Matrix", "scifi");
+
+            List<Movie> movies = new List<Movie>();
+            movies.Add(alien);
+            movies.Add(benjaminButton);
+            movies.Add(starWars);
+            movies.Add(cars);
+            movies.Add(insidious);
+            movies.Add(casablanca);
+            movies.Add(dune);
+            movies.Add(toyStory);
+            movies.Add(angryMen);
+            movies.Add(theMatrix);
+
+            Console.WriteLine("Welcome to Sean's movie list!");
+
+            while(cont == "y")
+            {
+                Console.WriteLine("Please input a genre type:");
+                string genre = Console.ReadLine().ToLower();
+
+                List<string> outputMovies = new List<string>();
+                foreach(Movie movie in movies)
+                {
+                    if(movie.Category == genre)
+                    {
+                        outputMovies.Add(movie.Title);
+                    }
+                }
+
+                if(outputMovies.Count >= 1)
+                {
+                    for(int i=0; i < outputMovies.Count; i++)
+                    {
+                        Console.WriteLine(outputMovies[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your input was invalid.");
+                }
+                Console.WriteLine("Would you like to try again? (y/n)");
+                cont = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine("Thank you for using our software. Have a wonderful day!");
+        }
+    }
+}
